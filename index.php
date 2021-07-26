ebdd1b9265e1ad9e1d9b8322b1df097217e1a81e
Esteban Gomez <juangomez1947@outlook.com>
2021-07-25 22:21:12 -0500

    Pagina principal index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anivision</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+    <?php require "php/model/assets/layout/header.php"?>
+    <?php require "php/model/assets/layout/footer.php"?>
+
+</body>
+</html>
