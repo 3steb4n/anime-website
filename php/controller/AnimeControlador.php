26bcf8eac718e6cb1983055e6c0201cb9afbc1ef
Esteban Gomez <juangomez1947@outlook.com>
2021-07-27 18:17:14 -0500

    Se agrega llamada del controlador por medio del botón del header como ejemplo

--- php/controller/AnimeControlador.php
+++ php/controller/AnimeControlador.php
@@ -3,7 +3,11 @@ require_once "php/model/CatalogoAnimeModelo.php";
 class AnimeControlador{
     public static function BusquedaAnime(){
         if(isset($_POST['buscarAnm'])){
-
+            echo "
+            <script>
+            alert('3232');
+            </script>
+            ";
         }else{
             $tabla = "anime";
             $respuesta = CatalogoAnimeModelo::ConsultarCatalogo($tabla, "", "");
