4a2185c54967c4f9befb5fa5fdcc3febd25544b1
Esteban Gomez <juangomez1947@outlook.com>
2021-07-27 18:20:59 -0500

    Se agrega funcion hover para el listado de animes y llamado del ajax

--- php/view/index.php
+++ php/view/index.php
@@ -8,7 +8,8 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
         body{
-            margin: 0;
+            margin: 0px;
+            height:100%;
             overflow-x: hidden;
         }
         h1{
@@ -17,31 +18,42 @@
         li{
             list-style:none; 
         }
+        .div-sinopsis{
+            transition-property: width, height, background-color;
+        }
         .div-sinopsis:hover{
-            background-color:rgba(99,99,99, 0.5);
-            background-position: top center;
+            background-position: center center;
+            background-size: 70px;  
             background-repeat: no-repeat;
-            background-image: url('https://img.icons8.com/material-outlined/192/000000/play--v2.png');
+            background-image: url('php/view/img/icon-play.png');
+            
         }
     </style>
 </head>
 <body>
+    <?php require_once "php/view/assets/layout/informacion.anime.php" ?>
     <?php require_once "php/view/assets/layout/header.php" ?>
     <?php require_once "php/controller/AnimeControlador.php" ?>
     <div style="width: 50vw; height: 100vh; margin: 0 auto;">
+    <form method="post">
     <?php 
     $resultado = AnimeControlador::BusquedaAnime();
     foreach($resultado as $dato){
-        echo "<div style='width: 240px;  height: 420px; border: 6px solid #eee; border-radius: 30px; float: left; margin: 10px; z-index: 4;'>"; 
-        echo "<div class='div-sinopsis' id='div-sinopsis' style='position: absolute; padding: 0; cursor: pointer; z-index: 3; width: 240px; border-radius: 30px;  height: 420px; float: left; '>";
+        echo "<div class='div-container' style='width: 235px; padding: 15px;  height: 420px; border: 6px solid #eee; border-radius: 30px; float: left; margin: 10px; z-index: 4;'>"; 
+        echo "<a href=javascript:consultarInformacion(".$dato['ID_ANIME'].")>";
+        echo "<div class='div-sinopsis' name='div-sinopsis' style='position: absolute; padding: 0; cursor: pointer; z-index: 3; width: 240px; height: 420px; float: left; transition: width 3s; transition-delay: 21s;'>";
         echo "</div>"; 
+        echo "</a>";
         echo "<img src=php/view/img/CatalogoAnimes/".$dato['IMG_ANIME']." style='height: 320px; width: 230px'; border-radius: 102px;'>";
-        echo "<h1 style='font-size: 15px;'>".$dato["NOMBRE_ANIME"]."</h1>";  
+        echo "<h1 class='anime-name' style='font-size: 15px;'>".$dato["NOMBRE_ANIME"]."</h1>";  
         echo "</div>";
     }
     ?>
+    </form>
     </div>
     <br><br><br><br><br>
     <?php require_once "php/view/assets/layout/footer.php" ?>
+<script src="php/view/js/jquery.js"></script>
+<script src="php/view/js/gestionar.anime.js"></script>
 </body>
 </html>
