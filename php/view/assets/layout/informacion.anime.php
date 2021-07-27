7da852470cfb6a6836391fef4d93f1f69d89dcae
Esteban Gomez <juangomez1947@outlook.com>
2021-07-27 18:18:46 -0500

    Se añade modal para añadir informacion adicional del anime seleccionado

---
+++ php/view/assets/layout/informacion.anime.php
@@ -0,0 +1,17 @@
+<div style="position: absolute; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); z-index: 8; display: none;">
+    <div style="position: absolute; width: 50%; height: 650px; background: #1f1f1f; right: 0; left: 0; margin: 0 auto;">
+    <div style="position: absolute; width: 50px; height: 50px; right: 0; top: 10px; z-index: 7; cursor: pointer;">
+    <label style="font-size: 50px; color: white; cursor: pointer;">X</label>
+    </div>
+    <div style="position: relative; width: 100%; height: 250px;">
+    <iframe width="100%" height="250" src="https://www.youtube.com/embed/W3q8Od5qJio" title="Treiler" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div style="position: relative; width: 150px; height: 40px; background: green; top: 10px;">
+    <input type="button" value="Ver Capitulos" style="width: 150px; height: 40px;">
+    </div>
+    <div style="position: relative; top: 20px; width: 100%; height: 200px; background: red; ">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum velit, 
+    magni iure suscipit beatae cum illum neque facilis porro vero nulla dolor eaque accusantium rerum, atque tempore nihil repellat sunt.
+    </div>
+    </div>
+</div>
