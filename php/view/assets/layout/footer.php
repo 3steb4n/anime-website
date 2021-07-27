d036b7440c8b9c581f81dbd67a6aee2baf027359
Esteban Gomez <juangomez1947@outlook.com>
2021-07-26 23:01:27 -0500

    Se arregla pie de pagina para el footer

---
+++ php/view/assets/layout/footer.php
@@ -0,0 +1,13 @@
+<div style="font-size: 15px; background: #eee;bottom: 0; border-top: 1px solid #ddd; height: 250px;">
+<div style="text-align: center; width: 50vw; height: 200px; margin: 0 auto;">
+    <h1 style="font-size:15px;">2021 Anishboard.com - Todos los derechos reservados.</h1>
+    <div style="float: left; background: white; border: 1px solid black; width: 150px; height: 35px; padding: 10px;">
+        <img src="php/view/img/discord.png" align="left" height="40px"><h1 style="font-size: 15px;">3st3b4n#3311</h1>
+    </div>
+    <a href="https://steamcommunity.com/id/cl0r0f0rm0/" target="_blank">
+    <div style="float: left; background: white; margin-left: 100px; border: 1px solid black; width: 150px; height: 35px; padding: 10px;">
+        <img src="php/view/img/steam-logo.png" align="left" height="40px"><h1 style="font-size: 15px;">Steam</h1>
+    </div>
+    </a>
+</div>
+</div>
