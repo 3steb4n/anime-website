5cbcc1e5a681ba8652eddcae04e5f669e16a89b6
Esteban Gomez <juangomez1947@outlook.com>
2021-07-28 22:55:28 -0500

    Se añade funcion para el funcionamiento del paginador

--- php/view/js/gestionar.anime.js
+++ php/view/js/gestionar.anime.js
@@ -1,13 +1,29 @@
+function cerrarModal(){
+    $('#trlAnm').attr('src', $('#trlAnm').attr('src'));
+    $('#infoAnm').css('display', 'none');
+}
+
 function consultarInformacion(idAnime){
     $.ajax({
         method: 'post',
         url: 'php/ajax/anime.ajax.php',
+        dataType: 'json',
         data: {
             'idAnimePost':idAnime
         },
         success: function(data){
             console.log(data);
-
+            $('#infoAnm').css('display', 'block');
+            $('#prfAnm').text(data[0]);
+            $('#trlAnm').attr('src', data[1]);
+            //alert(data[1]);
         }
     });
+}
+
+function organizarPaginas(){
+    let manyAnime=[
+        ['1', '2', '3', '4', '5', '6'],
+        ['7', '8', '9', '10', '11', '12']
+    ]
 }
