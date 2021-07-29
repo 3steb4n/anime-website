2fe3f4a9706103eb26ab5f3bfed8b50eaffc33d7
Esteban Gomez <juangomez1947@outlook.com>
2021-07-28 22:57:13 -0500

    Se añade logica para la busqueda de paginas no existentes

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-require_once "php/view/index.php";
+if(isset($_GET['ruta'])){
+    if($_GET['ruta']=='test1' || $_GET['ruta']=='test2'){
+        require_once "php/view/index.php";
+    }else{
+        require_once "php/view/assets/layout/error.notfound.php";
+    }
+}else{
+    require_once "php/view/index.php";
+}
 require_once "php/model/Conexion.php";
-require_once "php/model/CatalogoAnimeModelo.php";
+//require_once "php/model/CatalogoAnimeModelo.php";
+require_once "php/controller/AnimeControlador.php";
 ?>
