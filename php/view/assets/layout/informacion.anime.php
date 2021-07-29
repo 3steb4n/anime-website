ce71e5b9a098cd27e2ecae3d394398cf1de61879
Esteban Gomez <juangomez1947@outlook.com>
2021-07-28 22:54:03 -0500

    Se configura modal para que se mantega a la vista del usuario con el scroll

--- php/view/assets/layout/informacion.anime.php
+++ php/view/assets/layout/informacion.anime.php
@@ -1,17 +1,17 @@
-<div style="position: absolute; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); z-index: 8; display: none;">
+<div id="infoAnm" style="position: fixed; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); z-index: 8; display: none; overflow: hidden;">
     <div style="position: absolute; width: 50%; height: 650px; background: #1f1f1f; right: 0; left: 0; margin: 0 auto;">
+    <a href=javascript:cerrarModal()>
     <div style="position: absolute; width: 50px; height: 50px; right: 0; top: 10px; z-index: 7; cursor: pointer;">
-    <label style="font-size: 50px; color: white; cursor: pointer;">X</label>
     </div>
+    </a>
     <div style="position: relative; width: 100%; height: 250px;">
-    <iframe width="100%" height="250" src="https://www.youtube.com/embed/W3q8Od5qJio" title="Treiler" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="100%" height="250" id="trlAnm" title="Treiler" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div style="position: relative; width: 150px; height: 40px; background: green; top: 10px;">
+    <div style="position: relative; width: 150px; margin-left: 50px; height: 40px; background: green; top: 10px;">
     <input type="button" value="Ver Capitulos" style="width: 150px; height: 40px;">
     </div>
-    <div style="position: relative; top: 20px; width: 100%; height: 200px; background: red; ">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum velit, 
-    magni iure suscipit beatae cum illum neque facilis porro vero nulla dolor eaque accusantium rerum, atque tempore nihil repellat sunt.
+    <div style="position: relative; top: 20px; left:0; right: 0; margin: 0 auto; width: 90%; height: 200px; padding: 15px;">
+    <p id="prfAnm" style="font-size: 15px; font-family: 'Gowun Dodum', sans-serif; color: white;"></p>
     </div>
     </div>
 </div>
