06d109402c51440fbd34b5affca6e92b2b36c9b3
Esteban Gomez <juangomez1947@outlook.com>
2021-07-28 22:16:54 -0500

    Se agrega layout not found

---
+++ php/view/assets/layout/error.notfound.php
@@ -0,0 +1,33 @@
+<?php require_once 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        h1{
+            font-family: 'Open Sans', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div style="position: relative; width: 30%; height: 300px; border: 1px solid black; left: 0; right: 0; margin: 0 auto; top: 15px;">
+    <div style="position: relative; height: 40%; width: 250px;">
+        <img src="php/view/img/error-404.jpg" srcset="">
+    </div>
+    <div style="position: relative; left: 300px;">
+    <h1>404 Not Found</h1>
+    </div>
+    </div>
+</body>
+</html>
+
+<?php require_once 'footer.php'; ?>
