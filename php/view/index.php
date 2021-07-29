4b0612b804c40a0206082a38cdc157c10668beeb
Esteban Gomez <juangomez1947@outlook.com>
2021-07-28 22:56:03 -0500

    Se añade logica del paginador

--- php/view/index.php
+++ php/view/index.php
@@ -7,10 +7,14 @@
     <title>Anisboard</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap');
         body{
             margin: 0px;
             height:100%;
             overflow-x: hidden;
+        }
+        html{
+            height:100%;
         }
         h1{
             font-family: 'Open Sans', sans-serif;
@@ -34,11 +38,12 @@
     <?php require_once "php/view/assets/layout/informacion.anime.php" ?>
     <?php require_once "php/view/assets/layout/header.php" ?>
     <?php require_once "php/controller/AnimeControlador.php" ?>
-    <div style="width: 50vw; height: 100vh; margin: 0 auto;">
-    <form method="post">
+    <div style="position: relative; overflow: hidden; width: 50vw; margin: 0 auto; top: 15px; border: 1px solid black;">
     <?php 
-    $resultado = AnimeControlador::BusquedaAnime();
+    $paginador=0;
+    $resultado=AnimeControlador::BusquedaAnime();
     foreach($resultado as $dato){
+        $paginador+=1;
         echo "<div class='div-container' style='width: 235px; padding: 15px;  height: 420px; border: 6px solid #eee; border-radius: 30px; float: left; margin: 10px; z-index: 4;'>"; 
         echo "<a href=javascript:consultarInformacion(".$dato['ID_ANIME'].")>";
         echo "<div class='div-sinopsis' name='div-sinopsis' style='position: absolute; padding: 0; cursor: pointer; z-index: 3; width: 240px; height: 420px; float: left; transition: width 3s; transition-delay: 21s;'>";
@@ -49,9 +54,28 @@
         echo "</div>";
     }
     ?>
-    </form>
     </div>
-    <br><br><br><br><br>
+<div id="paginador" style="position: relative; width: 20%; height: 80px; background: green; top: 25px; margin: 0 auto;">
+  <div style="position: absolue; background: red; width: 40%; height: 60px; margin: 0 auto; top: 10px;">
+    <?php
+    $numeroPaginas=0;
+    $h=1;
+    for($i=1; $i<$paginador; $i++){
+        //echo $i;
+        $h+=1;
+        if($h==6){
+            $numeroPaginas+=1;
+            echo "<div style='position: relative; float: left; margin: 5px;'>";
+            echo "<p>".$numeroPaginas."</p>";
+            echo "</div>";
+            $h=0;
+        }
+        
+    }
+    //echo $paginador;
+    ?>
+    </div>
+</div>
     <?php require_once "php/view/assets/layout/footer.php" ?>
 <script src="php/view/js/jquery.js"></script>
 <script src="php/view/js/gestionar.anime.js"></script>
